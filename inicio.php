50f4c639ecfa76e5c27be5b380eb5d4eaff31149
Felipe Elorrieta <41742849+felipeelorrieta@users.noreply.github.com>
2019-07-11 13:36:32 -0400

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,55 @@
+<?php
+
+  session_start();
+  session_destroy();
+
+ ?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- para dispositivos moviles---->
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo.ico">    <!--imagen al princio de la pestaña---->
+    <!-- Bootstrap 4 archivos CSS -->
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!--archivos CSS personalizados -->
+    <link type="text/css" rel="stylesheet" href="css/estilo.css">
+
+    <!--Estilo css para que funcione el dropdown con submenu -->
+
+    <style>
+                  .dropdown-submenu {
+                  position: relative;
+                }
+
+                .dropdown-submenu a::after {
+                  transform: rotate(-90deg);
+                  position: absolute;
+                  right: 6px;
+                  top: .8em;
+                }
+
+                .dropdown-submenu .dropdown-menu {
+                  top: 0;
+                  left: 100%;
+                  margin-left: .1rem;
+                  margin-right: .1rem;
+                }
+        </style>
+
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+  </style>
+  
+      <title> Repositorio de Estadística</title> <!-- Nombre de la pagina---->
+  </head>
+  <body >
+    </body>
+</html>
